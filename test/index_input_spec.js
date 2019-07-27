fd87918d8efba506f3381a56fde9dd735623842f
Stefan R <huzergackl@gmail.com>
2019-07-27 17:32:57 +0200

    * Added more tests for the main logic

--- test/index_input_spec.js
+++ test/index_input_spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-named-capture-group */
+/* eslint-disable require-unicode-regexp */
 /* eslint-disable max-lines */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable prefer-destructuring */
@@ -34,13 +36,14 @@ const nodeRedModule = require('../index.js');
 
 // eslint-disable-next-line max-lines-per-function
 describe('index', function() {
+    // eslint-disable-next-line max-lines-per-function
     describe('input', function() {
-        it('should run through entire process with success', function() {
+        it('should run through entire process (within range)', function() {
             // arrange
             let result = null;
             const node = mock(nodeRedModule, {
                 startTime: '08:00',
-                endTime: '10:00',
+                endTime: '{{flow.flowKey}}',
                 startOffset: 1,
                 endOffset: 2,
                 lat: 51.33411,
@@ -72,6 +75,97 @@ describe('index', function() {
             // assert
             should.exist(result[0]);
             should.not.exist(result[1]);
+
+            const status = node.status();
+            status.fill.should.equal('green');
+            status.shape.should.equal('dot');
+            status.text.should.equal('08:01 - 10:02')
+        });
+        it('should run through entire process (outside range)', function() {
+            // arrange
+            let result = null;
+            const node = mock(nodeRedModule, {
+                startTime: '08:00',
+                endTime: '10:00',
+                startOffset: 1,
+                endOffset: 2,
+                lat: 51.33411,
+                lon: -0.83716,
+                unitTest: true
+            });
+            node.context = function() {
+                return {
+                    flow: {
+                        'keys'() { return ['flowKey']},
+                        'get'() { return '10:00' }
+                    },
+                    global: {
+                        'keys'() { return []},
+                        'get'() { return undefined }
+                    }
+                }
+            };
+            node.send = function(msg) {
+                result = msg;
+            };
+            node.now = function() {
+                return moment('2019-03-22 11:00:00');
+            };
+        
+            // act
+            node.emit('input', {});
+
+            // assert
+            should.not.exist(result[0]);
+            should.exist(result[1]);
+
+            const status = node.status();
+            status.fill.should.equal('green');
+            status.shape.should.equal('ring');
+            status.text.should.equal('08:01 - 10:02')
+        });
+        it('should run through entire process with suncalc (within range)', function() {
+            // arrange
+            let result = null;
+            const node = mock(nodeRedModule, {
+                startTime: '08:00',
+                endTime: 'goldenHour',
+                startOffset: 1,
+                endOffset: 2,
+                lat: 51.33411,
+                lon: -0.83716,
+                unitTest: true
+            });
+            node.context = function() {
+                return {
+                    flow: {
+                        'keys'() { return ['flowKey']},
+                        'get'() { return '10:00' }
+                    },
+                    global: {
+                        'keys'() { return []},
+                        'get'() { return undefined }
+                    }
+                }
+            };
+            node.send = function(msg) {
+                result = msg;
+            };
+            node.now = function() {
+                return moment('2019-03-22 11:00:00');
+            };
+        
+            // act
+            node.emit('input', {});
+
+            // assert
+            should.exist(result[0]);
+            should.not.exist(result[1]);
+
+            const status = node.status();
+            status.fill.should.equal('green');
+            status.shape.should.equal('dot');
+            /(\d+):(\d+) - (\d+):(\d+)/.test(status.text).should.be.true();
         });
     });
 });
